57094f0fd671cd00248e0e9eed6718b3fb272fc9
Nisyyyy <40120100+Nisyyyy@users.noreply.github.com>
2019-08-26 21:06:34 +0200

    Delete token.js

--- commands/token.js
+++
@@ -1,9 +0,0 @@
-module.exports.run = (bot, message, args) => {
-  let member = message.mentions.members.first();
-  message.channel.send(`LOL! ${member.username} got trolled by ${message.author.username}.`)
-  message.channel.send(`LOL! ${member.username} got trolled by ${message.member.username}.`)
-}
-
-module.exports.help = {
-  name: "troll"
-}
