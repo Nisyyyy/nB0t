07e3556f59e05f9a81cffe2cfb375f621e435b49
Nisyyyy <40120100+Nisyyyy@users.noreply.github.com>
2019-08-26 21:29:03 +0200

    Update guildCreate.js

--- events/guildCreate.js
+++ events/guildCreate.js
@@ -7,7 +7,7 @@ module.exports = (bot, guild, discord) => {
   const modc = guild.channels.find(c => c.name == "guild-logs")
   // post new server count
   snekfetch.post(`https://discordbots.org/api/bots/${bot.user.id}/stats`)
-    .set('Authorization', "NTkxNzMzMjc5NDQzNjQ4NTIy.XWMAEA.iQVDdFNAcNnhQb-5DX3zLMc-O1Y")
+    .set('Authorization', process.env.BOT_TOKEN)
     .send({ server_count: bot.guilds.size })
     .then(() => console.log('Updated discordbots.org stats.'))
     .catch(err => console.error(`Whoops something went wrong: ${err.body}`));
@@ -16,8 +16,8 @@ module.exports = (bot, guild, discord) => {
   let embed = new discord.RichEmbed()
   .setTitle("Introduction to nBot")
   .addField("Heyo! Thanks for adding me.","Before you start using me, I need to get some things straight. First of all, for some basic info about me, say `!info`.")
-  .addField("To see a list of commands","Say `!help`.", true)
-  .addField("If you need to report a bug:","go to the [issue tracker](https://github.com/FHGDev/JSnBot/issues/new), or just use the `!contact` command, and it will send a message straight to my creator.")
+  .addField("To see a list of commands","Say `nhelp`.", true)
+  .addField("If you need to report a bug:","go to the [issue tracker](https://github.com/Nisyyyy/nb0t), or just use the `ncontact` command, and it will send a message straight to my creator.")
   .addField("Done!", "Alrighty! Now you can start using all my awesome features in your server!")
   .setThumbnail(bot.user.avatarURL)
   .setTimestamp()
